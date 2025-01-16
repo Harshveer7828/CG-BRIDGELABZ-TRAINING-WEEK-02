8a65cfff9d8e4f7f3e7b14f58eee00b8739dbcc7
harshveer7828 <harshveer7828@gmail.com>
2025-01-16 15:40:21 +0530

    week-02 tasks done

---
+++ DAY-01/LEVEL-1/Book.java
@@ -0,0 +1,29 @@
+public class Book {
+    
+    //Create attributes 
+    String title;
+    String author;
+    int price;
+
+    //Create a constructor
+    Book(String title, String author, int price){
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+
+    //Display details 
+    void displayDetails(){
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("Price: " + price);
+    }
+
+    public static void main(String[] args) {
+        
+        //Create an object of the Book class
+        Book book = new Book("Death : An Inside Story", "Sadhguru", 250);
+        book.displayDetails();
+
+    }
+}
