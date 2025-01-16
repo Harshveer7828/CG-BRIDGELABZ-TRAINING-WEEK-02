465407f7a10289f5bfa3d082b1c068d8012e9ffc
harshveer7828 <harshveer7828@gmail.com>
2025-01-16 16:30:26 +0530

    WEEK2 task done

---
+++ DAY-01/LEVEL-2/CartItem.java
@@ -0,0 +1,27 @@
+/* Program to Simulate a Shopping Cart
+Problem Statement: Create a CartItem class with attributes itemName, price, and quantity. Add methods to:
+Add an item to the cart.
+Remove an item from the cart.
+Display the total cost.
+Explanation: The CartItem class models a shopping cart item. The methods handle cart operations like adding or removing items and calculating the total cost.
+ */
+
+public class CartItem {
+    // Declaring the attributes
+    String itemName;
+    int price;
+    int quantity;
+    
+    //Constructor to initialize the attributes
+    CartItem(String itemName, int price, int quantity){
+        this.itemName = itemName;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    // Method to Add an item to the cart.
+    void addToCart(String itemName, int price, int quantity){
+        
+    }
+
+}
---
+++ DAY-01/LEVEL-2/MovieTicket.java
@@ -0,0 +1,29 @@
+public class MovieTicket {
+    
+    //create attributes
+    String movieName;
+    int seatNumber;
+    int price;
+
+    //create constructor
+    MovieTicket(String movieName, int SeatNumber, int price){
+        this.movieName = movieName;
+        this.seatNumber = SeatNumber;
+        this.price = price;
+    }
+
+    //create a method to display the ticket details
+    void displayTicketDetails(){
+        System.out.println("Movie Name: " + movieName);
+        System.out.println("Seat Number: " + seatNumber);
+        System.out.println("Price: " + price);
+    }
+
+    public static void main(String[] args) {
+        
+        //Create an object of the MovieTicket class
+        MovieTicket ticket = new MovieTicket("Avengers : Endgame", 101, 5500);
+        ticket.displayTicketDetails();
+
+    }
+}
