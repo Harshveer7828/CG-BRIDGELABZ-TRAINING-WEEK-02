852429b5710a31bdd388327544fcd1cad50f6718
harshveer7828 <harshveer7828@gmail.com>
2025-01-18 16:28:53 +0530

    Day-02 Tasks are done

---
+++ Day-02/LEVEL-1/Common Problem/LiberaryBookSystem.java
@@ -0,0 +1,43 @@
+public class LiberaryBookSystem {
+    private String title;
+    private String author;
+    private double price;
+    private boolean isAvailable;
+
+    public LiberaryBookSystem(String title, String author, double price) {
+        this.title = title;
+        this.author = author;
+        this.price = price;
+        this.isAvailable = true;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void borrowBook() {
+        if (isAvailable) {
+            isAvailable = false;
+            System.out.println("Book borrowed successfully.");
+        } else {
+            System.out.println("Book is not available.");
+        }
+    }
+
+    public void returnBook() {
+        isAvailable = true;
+        System.out.println("Book returned successfully.");
+    }
+}
