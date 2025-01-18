1459461df1fc67b70227dc3dab590d3843527d92
harshveer7828 <harshveer7828@gmail.com>
2025-01-18 16:08:56 +0530

    Changes Done

---
+++ Day-02/LEVEL-1/Common Problem/HotelBooking.java
@@ -0,0 +1,51 @@
+public class HotelBooking {
+    private String guestName;
+    private String roomType;
+    private int nights;
+
+    // Default constructor
+    public HotelBooking() {
+        this.guestName = "";
+        this.roomType = "";
+        this.nights = 0;
+    }
+
+    // Parameterized constructor
+    public HotelBooking(String guestName, String roomType, int nights) {
+        this.guestName = guestName;
+        this.roomType = roomType;
+        this.nights = nights;
+    }
+
+    // Copy constructor
+    public HotelBooking(HotelBooking booking) {
+        this.guestName = booking.guestName;
+        this.roomType = booking.roomType;
+        this.nights = booking.nights;
+    }
+
+    // Getters and setters
+    public String getGuestName() {
+        return guestName;
+    }
+
+    public void setGuestName(String guestName) {
+        this.guestName = guestName;
+    }
+
+    public String getRoomType() {
+        return roomType;
+    }
+
+    public void setRoomType(String roomType) {
+        this.roomType = roomType;
+    }
+
+    public int getNights() {
+        return nights;
+    }
+
+    public void setNights(int nights) {
+        this.nights = nights;
+    }
+}
