e4ebb52e68ccfa113063e572a60041a8910704d2
harshveer7828 <harshveer7828@gmail.com>
2025-01-20 15:11:45 +0530

    Day-03 task is done

---
+++ Day-03/LEVEL-1/HospitalManagementSystem.java
@@ -0,0 +1,35 @@
+public class HospitalManagementSystem{
+    public static void main(String[] args) {
+        Patient patient1 = new Patient("Om Tiwari", 21, "STOMACH ALLERGY", 69);
+        if (patient1 instanceof Patient) {
+            patient1.displayPatientDetails();
+        }
+    }
+}
+
+class Patient {
+    static String hospitalName;
+    static int countPatient = 0;
+    private String name;
+    private int age;
+    private String ailment;
+    private final int patientId;
+    // Constructor to intitialize the attributes
+    Patient(String name, int age, String ailment, int patientId){
+        this.name = name;
+        this.age = age;
+        this.ailment = ailment;
+        this.patientId = patientId;
+    }
+    
+    // Method to display the details of the patient
+
+    void displayPatientDetails(){
+        System.out.println("Patien name :- "+ name +"\nPatient age:- "+age + "\nPatient ailment:- "+ailment+"\nPatient ID:- "+patientId);
+    }
+
+    // Method to display the total patients in the hospital
+    static void getTotalPatients(){
+        System.out.println("Toatal Patients are:- "+ countPatient);
+    }
+}
