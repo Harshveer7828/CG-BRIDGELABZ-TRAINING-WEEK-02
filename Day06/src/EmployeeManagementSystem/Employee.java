dab976ed5ed6251323c25ba563bae95e27a12497
harshveer7828 <harshveer7828@gmail.com>
2025-01-26 22:56:41 +0530

    Task day-06 done

--- Day06/src/EmployeeManagementSystem/Employee.java
+++ Day06/src/EmployeeManagementSystem/Employee.java
@@ -1,31 +1,45 @@
 package EmployeeManagementSystem;
 
-import java.util.ArrayList;
-
-public abstract class Employee {
-    // Required attributes to store the employee details
-    private String employeeId;
-    private long baseSalary;
+abstract class Employee {
+    private int employeeId;
     private String name;
-    public static ArrayList<Employee> employees;
+    private int baseSalary;
 
-    Employee(String employeeId, String name, long baseSalary){
+    public Employee(int employeeId, String name, int baseSalary) {
         this.employeeId = employeeId;
         this.name = name;
         this.baseSalary = baseSalary;
     }
 
-    // Public getter and setter for private attributes
-    public String getName(){
-        return name;
+    public abstract double calculateSalary();
+
+    public void displayDetails(){
+        System.out.println("Employee id : "+employeeId);
+        System.out.println("Employee Name : "+name);
+        System.out.println("Base Salary : "+baseSalary);
     }
-    public String getEmployeeId(){
+
+    public int getEmployeeId() {
         return employeeId;
     }
 
-    public void setBaseSalary(long baseSalary){
-        this.baseSalary = baseSalary;
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
     }
 
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getBaseSalary() {
+        return baseSalary;
+    }
+
+    public void setBaseSalary(int baseSalary) {
+        this.baseSalary = baseSalary;
+    }
 }
